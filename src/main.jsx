a4d343987d1643ced4cf4bec6a296d3217311060
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-01 00:30:38 +0200

    app fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import './styles/index.css';
 
+const baseName = '/camper';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename="/camper">
+        <BrowserRouter basename={baseName}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
