42addef8330e5ab18a563c7fb76bfb63693cffee
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-01 14:42:53 +0200

    App fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,38 @@
-import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
-import Home from '../Pages/Home/Home.jsx';
-import Catalog from '../Pages/Catalog/Catalog.jsx';
-import Favorites from '../Pages/Favorites/Favorites.jsx';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import Loader from '../components/Loader/Loader.jsx';
 import AppBar from './AppBar/AppBar.jsx';
-import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
 
-const App = () => {
-    const location = useLocation();
-    const baseName = location.pathname.split('/')[1];
+const Home = lazy(() => import('../Pages/Home/Home.jsx'));
+const Catalog = lazy(() => import('../Pages/Catalog/Catalog.jsx'));
+const Favorites = lazy(() => import('../Pages/Favorites/Favorites.jsx'));
 
+const appRoutes = [
+    { path: '/home', element: <Home /> },
+    { path: '/catalog', element: <Catalog /> },
+    { path: '/favorites', element: <Favorites /> },
+    { path: '*', element: <Navigate to="/home" /> },
+];
+
+const App = () => {
     return (
         <div className={css.container}>
             <AppBar />
             <div className={css.pageContainer}>
-                <Routes basename={baseName}>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/favorites" element={<Favorites />} />
-                    <Route path="*" element={<Navigate to="/home" />} />
-                </Routes>
+                <Suspense
+                    fallback={
+                        <div>
+                            <Loader />
+                        </div>
+                    }
+                >
+                    <Routes>
+                        {appRoutes.map(({ path, element }) => (
+                            <Route key={path} path={path} element={element} />
+                        ))}
+                    </Routes>
+                </Suspense>
             </div>
         </div>
     );
