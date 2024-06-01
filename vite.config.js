4eac6c6eeed34123f4836f6609b00e0e689f7936
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-01 19:56:13 +0200

    vite fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/campers/',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: '/src',
+            components: '/src/components',
+            pages: '/src/pages',
+            assets: '/src/assets',
+        },
+    },
+    base: '/campers',
 });
