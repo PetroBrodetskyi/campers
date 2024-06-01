47d74dd1d041977af34252e17c57f5a0c65bad76
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-01 15:21:20 +0200

    Update for GitHub Pages deployment

---
+++ 404.ml
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; URL=/" />
+    <script type="text/javascript">
+      // Redirect to root preserving path
+      var path = window.location.pathname.substr(1);
+      window.location.href = '/camper/' + path;
+    </script>
+  </head>
+  <body>
+  </body>
+</html>
